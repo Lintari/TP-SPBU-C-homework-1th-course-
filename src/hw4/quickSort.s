3de151ae7e66c819d67c5ef109a0787cd4179329
Luda Ashikhmina <ludaemalah@gmail.com>
2025-12-26 09:36:29 +0000

    added Assembley

---
+++ src/hw4/quickSort.s
@@ -0,0 +1,123 @@
+	.file	"quickSort.c"
+	.text
+	.globl	sort
+	.type	sort, @function
+sort:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	movl	%edx, -32(%rbp)
+	movl	-28(%rbp), %eax
+	cmpl	-32(%rbp), %eax
+	jge	.L9
+	movl	-32(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -8(%rbp)
+	movl	-28(%rbp), %eax
+	subl	$1, %eax
+	movl	%eax, -16(%rbp)
+	movl	-28(%rbp), %eax
+	movl	%eax, -12(%rbp)
+	jmp	.L4
+.L8:
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	cmpl	%eax, -8(%rbp)
+	jge	.L5
+	addl	$1, -12(%rbp)
+	jmp	.L4
+.L5:
+	addl	$1, -16(%rbp)
+	movl	-12(%rbp), %eax
+	cmpl	-16(%rbp), %eax
+	jle	.L7
+	movl	-12(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	(%rax), %eax
+	movl	%eax, -4(%rbp)
+	movl	-16(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rdx, %rax
+	movl	-12(%rbp), %edx
+	movslq	%edx, %rdx
+	leaq	0(,%rdx,4), %rcx
+	movq	-24(%rbp), %rdx
+	addq	%rcx, %rdx
+	movl	(%rax), %eax
+	movl	%eax, (%rdx)
+	movl	-16(%rbp), %eax
+	cltq
+	leaq	0(,%rax,4), %rdx
+	movq	-24(%rbp), %rax
+	addq	%rax, %rdx
+	movl	-4(%rbp), %eax
+	movl	%eax, (%rdx)
+.L7:
+	addl	$1, -12(%rbp)
+.L4:
+	movl	-12(%rbp), %eax
+	cmpl	-32(%rbp), %eax
+	jle	.L8
+	movl	-16(%rbp), %eax
+	leal	-1(%rax), %edx
+	movl	-28(%rbp), %ecx
+	movq	-24(%rbp), %rax
+	movl	%ecx, %esi
+	movq	%rax, %rdi
+	call	sort
+	movl	-16(%rbp), %eax
+	leal	1(%rax), %ecx
+	movl	-32(%rbp), %edx
+	movq	-24(%rbp), %rax
+	movl	%ecx, %esi
+	movq	%rax, %rdi
+	call	sort
+	jmp	.L1
+.L9:
+	nop
+.L1:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	sort, .-sort
+	.ident	"GCC: (Ubuntu 13.3.0-6ubuntu2~24.04) 13.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	1f - 0f
+	.long	4f - 1f
+	.long	5
+0:
+	.string	"GNU"
+1:
+	.align 8
+	.long	0xc0000002
+	.long	3f - 2f
+2:
+	.long	0x3
+3:
+	.align 8
+4:
